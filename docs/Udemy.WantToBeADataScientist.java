887c9790282a52b2684fa5aea4fc555b56ad02c7
anhvurz90 <anhvurz90@gmail.com>
2017-07-11 10:08:36 +0700

    Add doc: Udemy.WantToBeADataScientist.01.IntroductionToTheCourse

---
+++ docs/Udemy.WantToBeADataScientist.java
@@ -0,0 +1,40 @@
+UDEMY - WANT TO BE A DATA SCIENTIST?
+1.Introduction to the Course: {
+  + What this course is abount?
+  1.1.Data Scientist - demand: {
+    - Sexiest job of the 21st century
+    - The latest buzzword and the "in" thing
+    - Hot and happening field in software
+    - Significant projected demand for jobs across the world
+    - Everyong interested in becoming one
+  }
+  1.2.Questions in your mind: {
+    - What exactly is a data scientist?
+    - What does a data scientist do?
+    - What skills and training are required?
+    - What challenges does a data scientist face?
+    - Is it a good fit for me?
+    - How to acquire the skills required?
+    - Where to look for jobs?
+  }
+  1.3.Course Objective: {
+    - Answers to your questions are scattered around the web.
+    - Hard to find definitive answers.
+    - The course provides you one place to get all of them.!!
+    - This course explains the field of data science and what it takes to build skills for it.
+    - Its objective is to help you:
+      + Decide IF you want to become a data scientist
+      + Learn WHAT it takes to become one.
+  }
+  1.4.Expected Audience: {
+    - Anyone thinking about getting into Data Science
+    - Software Professionals
+    - Project Managers
+    - Mathematicians
+    - Business Domain Experts
+  }
+  1.5.What this course is not: {
+    - Any kind of technical training
+    - No software skills taught
+  }
+}
